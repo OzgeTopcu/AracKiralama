b4771c7973e0cb0d79603d9d957f1effc9e9d1c6
“Ozge <ozge2003topcu67@gmail.com>
2024-02-15 01:11:58 +0300

    9.Gün Ödevi Yapıldı

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,7 +1,9 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,27 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car,CarRentalContext>,ICarDal
     {
-       
+        public List<CarDetailDto> GetCarDetails()
+        {
+            using (CarRentalContext context = new CarRentalContext())
+            {
+                var result = from c in context.Cars
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             join clr in context.Colors
+                             on c.ColorId equals clr.ColorId
+                             select new CarDetailDto
+                             {
+                                 CarId = c.Id,
+                                 ColorName=clr.ColorName,
+                                 BrandName = b.BrandName,
+                                 Description=c.Description,
+                                 DailyPrice = c.DailyPrice,
+                                 
+                              
+                             };
+                return result.ToList();
+            }
+        }
     }
 }
