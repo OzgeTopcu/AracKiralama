8fd1e9c81658a6cc02906ad93677d57aa8d9b930
“Ozge <ozge2003topcu67@gmail.com>
2024-03-02 01:02:48 +0300

    12.Day finished

---
+++ Business/ValidationRules/FluentValidation/CarValidatior.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidatior:AbstractValidator<Car>
+    {
+        public CarValidatior() 
+        {
+
+            RuleFor(c => c.DailyPrice).GreaterThan(0);
+
+        }
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,57 +1,33 @@
-using Business.Abstract;
-using Business.Concrete;
-using DataAccess.Abstract;
-using DataAccess.Concrete.EntityFramework;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.DependencyResolves.Autofac;
 
+var builder = WebApplication.CreateBuilder(args);
 
-namespace apý
+// Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    builder.RegisterModule(new AutofacBusinessModule());
+});
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
-            // Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-            builder.Services.AddSingleton<ICarService, CarManager>();
-            builder.Services.AddSingleton<ICarDal,EfCarDal>();
-            builder.Services.AddSingleton<ICustomerService, CustomerManager>();
-            builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
-            builder.Services.AddScoped<IBrandService, BrandManager>();
-            builder.Services.AddScoped<IBrandDal, EfBrandDal>();
-            builder.Services.AddScoped<IColorService, ColorManager>();
-            builder.Services.AddScoped<IColorDal, EfColorDal>();
-            builder.Services.AddScoped<IRentalService, RentalManager>();
-            builder.Services.AddScoped<IRentalDal, EfRentalDal>();
-            builder.Services.AddScoped<IUserService, UserManager>();
-            builder.Services.AddScoped<IUserDal, EfUserDal>();
-
-
-
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
